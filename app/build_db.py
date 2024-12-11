8fa8ef6f5f7f2afa8ebed5629707d7efc1dfa199
Jonathan Metzler <jmetzler50@stuy.edu>
2024-12-11 11:40:48 -0500

    finished with bug testing for database, adding comments

--- app/build_db.py
+++ app/build_db.py
@@ -6,6 +6,7 @@
 
 import sqlite3
 import csv
+import os
 
 DB_FILE = "user.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
@@ -21,7 +22,6 @@ def makeDb():
     db = get_db()
     c = db.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)")
-    #change image back into blob later, is text rn for testing purposes
     c.execute("CREATE TABLE IF NOT EXISTS memes (id INTEGER PRIMARY KEY AUTOINCREMENT, image TEXT, upvotes INTEGER, creatingUsername TEXT NOT NULL, FOREIGN KEY (creatingUsername) REFERENCES users (username))")
     db.commit()
 
@@ -29,7 +29,7 @@ def makeDb():
 def addUser(u, p):
     db = get_db()
     c = db.cursor()
-    c.execute("SELECT FROM users WHERE username = ?", (u,))
+    c.execute("SELECT username FROM users WHERE username = ?", (u,))
     if c.fetchone() is not None:  # If the username already exists
         return False  # Return False
     else: #else add user
@@ -46,6 +46,9 @@ def addMeme(img, user):
         return False
     db = get_db()
     c = db.cursor()
+    c.execute("SELECT username FROM users WHERE username = ?",(user,))
+    if c.fetchone() is None:
+        return False
     c.execute("INSERT INTO memes (image, upvotes, creatingUsername) VALUES (?,0,?)",(img,user,))
     exportMemes()
     db.commit()
@@ -58,7 +61,7 @@ def getUserMemes(username):
     c.execute("SELECT id, image, upvotes FROM memes WHERE creatingUsername = ?", (username,))
     return c.fetchall()
 
-# Gets a specific meme based on id
+# Gets a specific meme based on id and returns it
 def getMeme(id):
     db = get_db()
     c = db.cursor()
@@ -69,7 +72,6 @@ def getMeme(id):
 def checkPass(user, p):
     db = get_db()
     c = db.cursor()
-
     c.execute("SELECT password FROM users WHERE username =  ?",(user,))
     if c.fetchone() is p:
         return True
@@ -77,7 +79,7 @@ def checkPass(user, p):
         return False
 
 # Gets a list of all memes
-def listAllMemes():
+def getAllMemes():
     db = get_db()
     c = db.cursor()
     c.execute("SELECT id, image, upvotes, creatingUsername FROM memes")
@@ -95,10 +97,10 @@ def deleteMeme(id):
 def deleteUser(user):
     db = get_db()
     c = db.cursor()
-    e.execute("SELECT FROM users WHERE username= ?",(user,))
+    c.execute("SELECT username FROM users WHERE username= ?",(user,))
     if c.fetchone() is None:
         return False
-    c.execute("SELECT meme FROM memes WHERE creatingUsername = ?", (user,))
+    c.execute("SELECT * FROM memes WHERE creatingUsername = ?", (user,))
     allMemes = [row[0] for row in c.fetchall()]
     for meme in allMemes:
         deleteMeme(meme)
@@ -111,7 +113,7 @@ def deleteUser(user):
 def upvote(id):
     db = get_db()
     c = db.cursor()
-    c.execute("SELECT meme FROM memes WHERE id = ?", (id,))
+    c.execute("SELECT * FROM memes WHERE id = ?", (id,))
     if c.fetchone() is None:
         return False
     else:
@@ -135,8 +137,17 @@ def exportUsers():
 def exportMemes():
     exportToCSV("SELECT * FROM memes", 'memes.csv')
 
-makeDb()
 
-#testing ground
+
+
+#COMMENT THIS OUT LATER WHEN FINAL PRODUCT
+#if os.path.exists("user.db"):
+#    os.remove("user.db")
+
+
+
+
+
+makeDb()
 
 db.close()
